eafd0f4530e0c60803487e4c07cd2d777a445e3b
William Tetlow <william@getshogun.com>
2021-07-28 12:17:23 +0100

    bug with webpack5 and babel ignore

--- pages/a-page/index.js
+++ pages/a-page/index.js
@@ -1,10 +1,13 @@
-import Link from 'next/link'
+import Link from "next/link";
+import "../../testing/test";
 
 export default function APage() {
-  return <div>
-    <h1>This is a page - go back to homepage</h1>
-    <Link href="/">
-      <a>Click here</a>
-    </Link>
-  </div>
+  return (
+    <div>
+      <h1>This is a page - go back to homepage</h1>
+      <Link href="/">
+        <a>Click here</a>
+      </Link>
+    </div>
+  );
 }
